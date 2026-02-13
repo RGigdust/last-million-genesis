b79f6ecf0ad682c2afd1be69458dd1d36a8dcf23
RGigdust <hio474088@gmail.com>
2026-02-13 06:50:56 +0200

    Update ai.js

--- ai.js
+++ ai.js
@@ -1,1 +1,16 @@
+let hunterDistance = 100;
 
+function hunterAI(){
+
+hunterDistance -= Math.random() * 0.5;
+
+if(hunterDistance < 20){
+
+document.getElementById("status").innerHTML =
+"Hunter Near";
+
+}
+
+}
+
+setInterval(hunterAI, 1000);
